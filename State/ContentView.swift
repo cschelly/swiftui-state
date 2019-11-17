fd2a618c9c5acc5125d1468401de17da65322b4e
Christina <christina.sund@tealium.com>
2019-11-16 19:52:56 -0800

    Initial Commit

---
+++ State/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  State
+//
+//  Created by Christina S on 11/16/19.
+//  Copyright © 2019 Schelly. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
